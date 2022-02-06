df0c36ff52d1495eb91db453fc17a5d69a21dd9e
SimpleCode <dowon2308@gmail.com>
2022-02-06 16:16:10 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php
 /**
  * Index Page
- * @author Simplecode <simplecode@sensitivecms.com>
- * @link http://sensitivecms.com
+ * @author SimpleCode <smaker@simplesoftcorp.com>
+ * @link https://simplesoftcorp.com
  */
 
 // PHP 버전을 확인하고, 이를 만족하지 못하면 실행을 중단한다.
-if(version_compare(PHP_VERSION, '5.3.3', '<'))
+if(version_compare(PHP_VERSION, '7.4.0', '<'))
 {
 	header('Content-Type: text/html; charset=utf-8');
 	echo '현재 사용중인 PHP 버전은 호환되지 않는 버전이기 때문에 어플리케이션 실행을 중단합니다. PHP 버전을 업데이트 해주세요.';
