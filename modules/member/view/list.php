1f0b1c1d0552bf48cd0a1465b3f7463effaac9e7
SimpleCode <dowon2308@gmail.com>
2022-02-06 16:01:38 +0900

    member 모듈 문법 오류 수정

--- modules/member/view/list.php
+++ modules/member/view/list.php
@@ -14,40 +14,40 @@
 			</th>
 		</tr>
 	</thead>
-  <tbody>
-	{{ @foreach ($list as $no => $member) }}
-	<tr id="member_{{ $member->memberId }}">
-		<td>{{ escape($member->userId) }}</td>
-		<td>{{ escape($member->userName) }}</td>
-		<td>
-			<?php
-			switch($member->status)
-			{
-				case 'STANDBY':
-					echo \Module\Member\AdminView::STATUS_STANDBY;
-					break;
-				case 'APPROVED':
-					echo \Module\Member\AdminView::STATUS_APPROVED;
-					break;
-				case 'DENIED':
-				echo \Module\Member\AdminView::STATUS_DENIED;
-					break;
-			}
-			?>
-		</td>
-		<td>{{ convertDateFormat($member->createdAt, 'Y-m-d') }}</td>
-		<td>{{ convertDateFormat($member->loginAt, 'Y-m-d') }}</td>
-		<td>
-			<a href="/admin/member/edit/{{ $member->memberId }}">조회/수정</a>
-		</td>
-		<td>
-			<div class="ui ui-popup checkbox check"{{ @if ($member->isAdmin == 'Y') }} data-content="최고 관리 권한을 가진 회원은 선택할 수 없습니다."{{ @end }}>
-				<input type="checkbox" name="cart" value="{{ $member->memberId }}"<?php if ($member->isAdmin == 'Y') { ?> disabled{{ @endif }}> <label></label>
-			</div>
-		</td>
-	</tr>
-	{{ @endforeach }}
-  </tbody>
+	<tbody>
+		{{ @foreach ($list as $no => $member) }}
+		<tr id="member_{{ $member->memberId }}">
+			<td>{{ escape($member->userId) }}</td>
+			<td>{{ escape($member->userName) }}</td>
+			<td>
+				<?php
+				switch($member->status)
+				{
+					case 'STANDBY':
+						echo \Module\Member\AdminView::STATUS_STANDBY;
+						break;
+					case 'APPROVED':
+						echo \Module\Member\AdminView::STATUS_APPROVED;
+						break;
+					case 'DENIED':
+						echo \Module\Member\AdminView::STATUS_DENIED;
+						break;
+				}
+				?>
+			</td>
+			<td>{{ convertDateFormat($member->createdAt, 'Y-m-d') }}</td>
+			<td>{{ convertDateFormat($member->loginAt, 'Y-m-d') }}</td>
+			<td>
+				<a href="/admin/member/edit/{{ $member->memberId }}">조회/수정</a>
+			</td>
+			<td>
+				<div class="ui ui-popup checkbox check"{{ @if ($member->isAdmin == 'Y') }} data-content="최고 관리 권한을 가진 회원은 선택할 수 없습니다."{{ @end }}>
+					<input type="checkbox" name="cart" value="{{ $member->memberId }}"<?php if ($member->isAdmin == 'Y'): ?> disabled <?php endif; ?>> <label></label>
+				</div>
+			</td>
+		</tr>
+		{{ @endforeach }}
+	</tbody>
 </table>
 
 <div class="ui left floated basic segment" style="padding:0">
@@ -65,24 +65,23 @@ $('.check').checkbox('attach events', '.checkbox.cart', 'toggle');
 
 <div class="ui dimmer modals page transition hidden">
 	<div class="ui small basic deleteConfirm modal transition">
-	   	<div class="ui icon header">
+		<div class="ui icon header">
 			<i class="trash icon"></i>
 			회원 삭제
     </div>
     <div class="content">
-    	<p style="text-align:center">한 번 삭제하면 되돌릴 수 없습니다. 정말 삭제하시겠습니까?</p>
+		<p style="text-align:center">한 번 삭제하면 되돌릴 수 없습니다. 정말 삭제하시겠습니까?</p>
     </div>
     <div class="actions" style="text-align: center">
 		<div class="ui green ok inverted button">
-       		<i class="checkmark icon"></i>
-       		삭제하기
-       	</div>
-       	<div class="ui red basic cancel inverted button">
-       		<i class="remove icon"></i>
-       		취소하기
-       	</div>
+			<i class="checkmark icon"></i>
+			삭제하기
+		</div>
+		<div class="ui red basic cancel inverted button">
+			<i class="remove icon"></i>
+			취소하기
+		</div>
     </div>
-  </div>
 </div>
 
 <div style="clear:both"></div>
